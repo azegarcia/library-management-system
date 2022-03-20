4a79b6bc638dff7150ab08d8d35c47a474ed7bbd
Aze Garcia <aze.garcia28@gmail.com>
2022-03-20 23:27:44 +0800

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,56 @@
+		
+    	<div style="overflow:hidden; width:960px; margin:0 auto; padding:0 20px;"> 
+                <div class="pix_diapo">
+
+                    <div data-thumb="LMS/LIB1.png">
+                        <img src="LMS/LIB1.png">
+                    </div>
+                    
+                    <div data-thumb="LMS/LIB2.png">
+                        <img src="LMS/LIB2.png"> 
+                    </div>
+                    
+                    <div data-thumb="LMS/LIB3.png" data-time="7000">
+                        <img src="LMS/LIB3.png">
+                    </div>
+					
+					<div data-thumb="LMS/LIB4.png">
+                        <img src="LMS/LIB4.png"> 
+                    
+                    </div>
+					<div data-thumb="LMS/LIB5.png">
+                        <img src="LMS/LIB5.png"> 
+                    
+                    </div>
+					<div data-thumb="LMS/LIB6.png">
+                        <img src="LMS/LIB6.png"> 
+                    
+                    </div>
+					<div data-thumb="LMS/LIB7.png">
+                        <img src="LMS/LIB7.png"> 
+                    </div>
+					
+					<div data-thumb="LMS/LIB8.png">
+                        <img src="LMS/LIB8.png"> 
+                    </div>
+					
+					<div data-thumb="LMS/LIB9.png">
+                        <img src="LMS/LIB9.png"> 
+                    </div>
+					<div data-thumb="LMS/LIB10.png">
+                        <img src="LMS/LIB10.png"> 
+                    </div>
+				
+					
+					
+				
+  
+                    
+      
+                    
+               </div><!-- #pix_diapo -->
+                
+        </div>
+    
+    
+    </section> 
